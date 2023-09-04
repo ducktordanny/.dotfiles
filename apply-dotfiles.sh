82b952da916fad3c08916f4335aa3ab65ad6b450
Daniel Lazar <lazardani0112@gmail.com>
2023-09-04 23:43:06 +0200

    Simplify apply script

--- apply-dotfiles.sh
+++ apply-dotfiles.sh
@@ -4,22 +4,17 @@
 
 script_path="$(readlink -f "$0")"
 script_directory="$(dirname "$script_path")"
-current_directory="$(pwd)"
 
-if [ "$script_directory" = "$current_directory" ]; then
-    folders=()
-    for item in */; do
-        if [ -d "$item" ]; then
-            folders+=("$item")
-        fi
-    done
-    for folder in "${folders[@]}"; do
-        cp -r ./$folder ~/.config/$folder
-    done
+folders=()
+for item in */; do
+    if [ -d "$item" ]; then
+        folders+=("$item")
+    fi
+done
+for folder in "${folders[@]}"; do
+    cp -r $script_directory/$folder ~/.config/$folder
+done
 
-    tmux source ~/.config/tmux/tmux.conf
-    yabai --restart-service
-    skhd --restart-service
-else
-    echo "The script should run from its own directory."
-fi
+tmux source ~/.config/tmux/tmux.conf
+yabai --restart-service
+skhd --restart-service
