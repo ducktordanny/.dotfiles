53ac3671a704754fa78fa241f9132928f341980c
Daniel Lazar <lazardani0112@gmail.com>
2023-10-14 23:20:41 +0200

    NeoVim: Keymap for telescope history cylce

--- nvim/lua/ducktordanny/remaps/telescope.lua
+++ nvim/lua/ducktordanny/remaps/telescope.lua
@@ -13,6 +13,8 @@ local telescope_mappings = {
   ['<C-d>'] = 'delete_buffer',
   ['<C-j>'] = actions.move_selection_next,
   ['<C-k>'] = actions.move_selection_previous,
+  ['<C-l>'] = actions.cycle_history_next,
+  ['<C-h>'] = actions.cycle_history_prev,
 }
 
 telescope.setup {
