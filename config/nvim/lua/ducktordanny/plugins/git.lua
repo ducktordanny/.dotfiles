3f026fcad8db4048a8ee655fa8a979d9675f088a
Daniel Lazar <lazardani0112@gmail.com>
2023-11-01 11:52:23 +0100

    NeoVim: Fix Gitsigns not loading

--- config/nvim/lua/ducktordanny/plugins/git.lua
+++ config/nvim/lua/ducktordanny/plugins/git.lua
@@ -2,19 +2,19 @@ return {
   {
     "tpope/vim-fugitive",
     config = function()
-      vim.keymap.set('n', '<leader>ga', '<cmd>Git add --all<cr>', { desc = '[G]it add [a]ll' })
-      vim.keymap.set('n', '<leader>gc', '<cmd>Git commit -m ""', { desc = '[G]it [C]ommit' })
-      vim.keymap.set('n', '<leader>gs', '<cmd>Git status<cr>', { desc = '[G]it [S]tatus' })
-      vim.keymap.set('n', '<leader>gp', '<cmd>Git push', { desc = '[G]it [P]ush' })
-      vim.keymap.set('n', '<leader>gf', '<cmd>Git fetch', { desc = '[G]it [F]etch' })
-      vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<cr>', { desc = '[G]it [B]lame' })
-      vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<cr>', { desc = '[G]it [D]iff preview' })
+      vim.keymap.set("n", "<leader>ga", "<cmd>Git add --all<cr>", { desc = "[G]it add [a]ll" })
+      vim.keymap.set("n", "<leader>gc", '<cmd>Git commit -m ""', { desc = "[G]it [C]ommit" })
+      vim.keymap.set("n", "<leader>gs", "<cmd>Git status<cr>", { desc = "[G]it [S]tatus" })
+      vim.keymap.set("n", "<leader>gp", "<cmd>Git push", { desc = "[G]it [P]ush" })
+      vim.keymap.set("n", "<leader>gf", "<cmd>Git fetch", { desc = "[G]it [F]etch" })
+      vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<cr>", { desc = "[G]it [B]lame" })
+      vim.keymap.set("n", "<leader>gd", "<cmd>Gdiffsplit<cr>", { desc = "[G]it [D]iff preview" })
     end,
   },
   {
     "rbong/vim-flog",
     config = function()
-      vim.keymap.set('n', '<leader>gl', '<cmd>Flogsplit<cr>', { desc = '[G]it [L]og' })
+      vim.keymap.set("n", "<leader>gl", "<cmd>Flogsplit<cr>", { desc = "[G]it [L]og" })
     end,
   },
   {
@@ -29,8 +29,9 @@ return {
       },
     },
     config = function()
-      vim.keymap.set('n', '<leader>gu', '<cmd>Gitsigns reset_hunk<cr>', { desc = '[G]it [U]ndo hunk' })
-      vim.keymap.set('n', '<leader>gr', '<cmd>Gitsigns refresh<cr>', { desc = '[G]itsigns [R]efresh' })
+      require("gitsigns").setup() -- NOTE: This is needed, otherwise it won't load for some reason
+      vim.keymap.set("n", "<leader>gu", "<cmd>Gitsigns reset_hunk<cr>", { desc = "[G]it [U]ndo hunk" })
+      vim.keymap.set("n", "<leader>gr", "<cmd>Gitsigns refresh<cr>", { desc = "[G]itsigns [R]efresh" })
     end,
   },
   {
@@ -41,7 +42,7 @@ return {
     },
     config = function()
       require("telescope").load_extension "lazygit"
-      vim.keymap.set('n', '<leader>l', '<cmd>LazyGit<cr>', { desc = '[L]azyGit' })
+      vim.keymap.set("n", "<leader>l", "<cmd>LazyGit<cr>", { desc = "[L]azyGit" })
     end,
   },
 }
