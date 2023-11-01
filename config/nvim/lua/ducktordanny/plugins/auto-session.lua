e55cb094fdd49dfa33ab9decb4d3c768daf4594d
Daniel Lazar <lazardani0112@gmail.com>
2023-11-01 11:55:12 +0100

    NeoVim: Keymap for restoring session

--- config/nvim/lua/ducktordanny/plugins/auto-session.lua
+++ config/nvim/lua/ducktordanny/plugins/auto-session.lua
@@ -1,10 +1,11 @@
 return {
-  'rmagatti/auto-session',
+  "rmagatti/auto-session",
   config = function()
-    require('auto-session').setup {
-      log_level = 'error',
+    require("auto-session").setup {
+      log_level = "error",
       auto_restore_enabled = false,
-      auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
     }
+    vim.keymap.set("n", "<leader>ar", "<cmd>SessionRestore", { desc = "[A]uto-session [R]estore" })
   end,
 }
