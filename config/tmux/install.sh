b35a54fec2b4d576fe6a1393135c78e4c18c603d
Daniel Lazar <lazardani0112@gmail.com>
2023-11-11 17:37:05 +0100

    Build: Improve install scripts

--- config/tmux/install.sh
+++ config/tmux/install.sh
@@ -2,4 +2,6 @@
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 brew install tmux
-tmux
+
+echo "tmux installed ✅"
+echo "After starting tmux install plugins: prefix+I"
--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ echo "Installing brew..."
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "Installing basic packages..."
-brew install node git nvm neovim tmux koekeishiya/formulae/yabai koekeishiya/formulae/skhd ripgrep docker
+brew install node git nvm neovim koekeishiya/formulae/yabai koekeishiya/formulae/skhd ripgrep docker
 npm install -g yarn
 
 echo "Installing nerd fonts..."
@@ -24,8 +24,10 @@ curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%
 
 echo "Installing terminal related things..."
 brew install --cask iterm2
-brew install powerlevel10k
+brew install powerlevel10k zsh-autosuggestions zsh-syntax-highlighting
 echo "source $(brew --prefix)/share/powerlevel10k/powerlevel10k.zsh-theme" >>~/.zshrc
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+echo "source $(brew --prefix)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 echo "Installing GUI applications..."
 brew install --casks istat-menus karabiner-elements alfred insomnia discord arc figma microsoft-teams gimp vlc obs coconutbattery
@@ -34,7 +36,19 @@ echo "Clone dotfiles repo..."
 git clone https://github.com/ducktordanny/.dotfiles ~/.config/.dotfiles
 cd ~/.config/.dotfiles
 
-echo "Applying dotfile configs..."
-bash apply-dotfiles.sh
+bash ~/.config/.dotfiles/config/tmux/install.sh
+
+echo "Start services for yabai and skhd"
+yabai --start-service
+skhd --start-service
 
 echo "Done. ✅"
+
+echo "Next steps:"
+echo -e "\t- Quit current terminal and open iTerm."
+echo -e "\t- Set up powerlevel10k preferences (it will come up right away)."
+echo -e "\t- Then cd into ~/.config/.dotfiles and run 'bash apply-dotfiles'"
+
+echo "Some other stuff:"
+echo -e "\t- Add necessary access to some application like Karabiner-Elements."
+echo -e "\t- Create hyper key in Karabiner-Elements under Complex modifications."
