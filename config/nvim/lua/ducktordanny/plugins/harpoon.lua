f68d6eac737ef8f395c3b9c2b698161e32e5b11b
Daniel Lazar <lazardani0112@gmail.com>
2023-12-18 20:37:04 +0100

    NeoVim: Extra keymaps for harpoon

--- config/nvim/lua/ducktordanny/plugins/harpoon.lua
+++ config/nvim/lua/ducktordanny/plugins/harpoon.lua
@@ -19,5 +19,11 @@ return {
     vim.keymap.set("n", "<leader>hh", harpoon_mark.add_file)
     vim.keymap.set("n", "<C-n>", harpoon_ui.nav_next)
     vim.keymap.set("n", "<C-p>", harpoon_ui.nav_prev)
+
+    vim.keymap.set("n", "<leader>1", function() harpoon_ui.nav_file(1) end, { desc = "Navigate to harpoon file 1" })
+    vim.keymap.set("n", "<leader>2", function() harpoon_ui.nav_file(2) end, { desc = "Navigate to harpoon file 2" })
+    vim.keymap.set("n", "<leader>3", function() harpoon_ui.nav_file(3) end, { desc = "Navigate to harpoon file 3" })
+    vim.keymap.set("n", "<leader>4", function() harpoon_ui.nav_file(4) end, { desc = "Navigate to harpoon file 4" })
+    vim.keymap.set("n", "<leader>5", function() harpoon_ui.nav_file(5) end, { desc = "Navigate to harpoon file 5" })
   end,
 }
