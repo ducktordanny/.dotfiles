d66847f8bcab14576f8dbdf64d392d4d9a0fe35e
Daniel Lazar <lazardani0112@gmail.com>
2024-03-05 21:39:19 +0100

    SketchyBar: Add extra space for media

--- config/sketchybar/plugins/media.sh
+++ config/sketchybar/plugins/media.sh
@@ -3,7 +3,7 @@
 STATE="$(echo "$INFO" | jq -r '.state')"
 if [ "$STATE" = "playing" ]; then
   MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
-  sketchybar --set $NAME label="$MEDIA" drawing=on
+  sketchybar --set $NAME label="$MEDIA " drawing=on
 else
   sketchybar --set $NAME label="Not playing" drawing=on
 fi
