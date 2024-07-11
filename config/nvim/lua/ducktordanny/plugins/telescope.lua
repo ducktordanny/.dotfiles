55cd67963770ab800c67272e3d34a5d6aa06f7f7
Daniel Lazar <lazardani0112@gmail.com>
2024-07-11 22:23:31 +0200

    NeoVim: Use telescope.builtin.git_files for file search

--- config/nvim/lua/ducktordanny/plugins/telescope.lua
+++ config/nvim/lua/ducktordanny/plugins/telescope.lua
@@ -37,7 +37,8 @@ return {
       -- See `:help telescope.builtin`
       vim.keymap.set("n", "<leader><space>", builtin.buffers, { desc = "[ ] Find existing buffers" })
       vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "[/] Fuzzily search in current buffer" })
-      vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+      vim.keymap.set("n", "<leader>sf", builtin.git_files, { desc = "[S]earch Git [F]iles" })
+      vim.keymap.set("n", "<leader>sff", builtin.find_files, { desc = "[S]earch [F]iles" })
       vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
       vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
       vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
