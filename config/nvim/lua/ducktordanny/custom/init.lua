fb1c4dbae8a06071e93473f423bf67bb9957f1a7
Daniel Lazar <lazardani0112@gmail.com>
2024-07-14 17:36:03 +0200

    NeoVim: [poc] Clear unnecessary buffers
    Too many open buffers may cause lsp to run into issues (at least that's
    what I heard), so I try to limit the maximum number of open buffers, if
    it exceeds the maximum then the last one gets deleted.

--- config/nvim/lua/ducktordanny/custom/init.lua
+++ config/nvim/lua/ducktordanny/custom/init.lua
@@ -1,3 +1,4 @@
+require "ducktordanny.custom.close-old-buffers"
 local worktree = require "ducktordanny.custom.worktree"
 
 vim.keymap.set("n", "<leader>si", worktree.select_worktree_dropdown, { desc = "Select work tree" })
