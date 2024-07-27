35a2dd7dde3b1c9ef03604a380737fd05d92768f
Daniel Lazar <lazardani0112@gmail.com>
2024-07-27 16:03:36 +0200

    NeoVim: Remove buffer closing script
    Removing it as it was not working as expected. It confused the LSP
    sometimes, by closing random buffers.

--- config/nvim/lua/ducktordanny/custom/init.lua
+++ config/nvim/lua/ducktordanny/custom/init.lua
@@ -1,4 +1,3 @@
-require "ducktordanny.custom.close-old-buffers"
 local worktree = require "ducktordanny.custom.worktree"
 
 vim.keymap.set("n", "<leader>si", worktree.select_worktree_dropdown, { desc = "Select work tree" })
