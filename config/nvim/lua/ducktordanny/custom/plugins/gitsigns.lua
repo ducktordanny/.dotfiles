6a37c7c6e2f758e3bc6f4e94fe42288e152b07eb
Daniel Lazar <lazardani0112@gmail.com>
2024-08-08 20:20:00 +0200

    NeoVim: Git hunk inline preview keymap

--- config/nvim/lua/ducktordanny/custom/plugins/gitsigns.lua
+++ config/nvim/lua/ducktordanny/custom/plugins/gitsigns.lua
@@ -1,3 +1,4 @@
 vim.keymap.set("n", "<leader>gu", "<cmd>Gitsigns reset_hunk<cr>", { desc = "[G]it [U]ndo hunk" })
+vim.keymap.set("n", "<leader>gj", "<cmd>Gitsigns preview_hunk_inline<cr>", { desc = "Git hunk inline preview" })
 vim.keymap.set("v", "ga", "<cmd>Gitsigns stage_hunk<cr>", { desc = "Stage selected hunk" })
 vim.keymap.set("v", "gr", "<cmd>Gitsigns undo_stage_hunk<cr>", { desc = "Undo stage for selected hunk" })
