90f0e5613c6a60b76ab9fd76d6cdfe28e7d462a0
Daniel Lazar <lazardani0112@gmail.com>
2024-11-19 19:40:09 +0100

    NeoVim: Add random explanation comment

--- config/nvim/lua/ducktordanny/plugins/which-key.lua
+++ config/nvim/lua/ducktordanny/plugins/which-key.lua
@@ -1,6 +1,7 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  -- I don't like the new version
   commit = "ccf0276",
   dependencies = { "echasnovski/mini.nvim", "nvim-tree/nvim-web-devicons" },
   opts = {},
