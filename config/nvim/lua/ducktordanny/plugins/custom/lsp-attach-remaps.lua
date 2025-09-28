1096d26e0a06db3531920ccdb3577d48f38b28ca
Daniel Lazar <lazardani0112@gmail.com>
2025-09-28 21:13:42 +0200

    NeoVim: Refactor LSP remaps into separate file.

---
+++ config/nvim/lua/ducktordanny/plugins/custom/lsp-attach-remaps.lua
@@ -0,0 +1,43 @@
+local M = {}
+
+M.on_attach = function(_, bufnr)
+  -- In this case, we create a function that lets us more easily define mappings specific
+  -- for LSP related items. It sets the mode, buffer and description for us each time.
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = "LSP: " .. desc
+    end
+
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap("<leader>rn", function()
+    local cword = vim.fn.expand "<cword>"
+    local new_name = vim.fn.input("Rename to: ", cword)
+    if new_name and #new_name > 0 and new_name ~= cword then
+      vim.lsp.buf.rename(new_name) -- name is provided -> no more extra prompts
+    end
+  end, "[R]e[n]ame")
+  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+
+  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
+  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+
+  -- See `:help K` for why this keymap
+  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+  nmap("KK", vim.lsp.buf.signature_help, "Signature Documentation")
+
+  -- Lesser used LSP functionality
+  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+    vim.lsp.buf.format()
+  end, { desc = "Format current buffer with LSP" })
+end
+
+return M
--- config/nvim/lua/ducktordanny/plugins/custom/lsp.lua
+++ config/nvim/lua/ducktordanny/plugins/custom/lsp.lua
@@ -1,43 +1,3 @@
-local on_attach = function(_, bufnr)
-  -- In this case, we create a function that lets us more easily define mappings specific
-  -- for LSP related items. It sets the mode, buffer and description for us each time.
-  local nmap = function(keys, func, desc)
-    if desc then
-      desc = "LSP: " .. desc
-    end
-
-    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-  end
-
-  nmap("<leader>rn", function()
-    local cword = vim.fn.expand "<cword>"
-    local new_name = vim.fn.input("Rename to: ", cword)
-    if new_name and #new_name > 0 and new_name ~= cword then
-      vim.lsp.buf.rename(new_name) -- name is provided -> no more extra prompts
-    end
-  end, "[R]e[n]ame")
-  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
-  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-  nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
-  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
-  nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-  nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-  -- See `:help K` for why this keymap
-  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-  nmap("KK", vim.lsp.buf.signature_help, "Signature Documentation")
-
-  -- Lesser used LSP functionality
-  nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-
-  -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-    vim.lsp.buf.format()
-  end, { desc = "Format current buffer with LSP" })
-end
-
 -- NOTE: Node should be installed on system for most of these LSPs
 local servers = {
   lua_ls = {
@@ -101,7 +61,7 @@ vim.o.winborder = "rounded"
 for server_name, server_options in pairs(servers or {}) do
   local common_options = {
     capabilities = capabilities,
-    on_attach = on_attach,
+    on_attach = require("ducktordanny.plugins.custom.lsp-attach-remaps").on_attach,
     flags = { debounce_text_changes = 150 },
   }
   local options = vim.tbl_deep_extend("force", common_options, server_options);
